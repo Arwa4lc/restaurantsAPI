888a75a6a73deec5c506ccdeed64dbbb72db03f3
Arwa Abdelrahem <arwaabdelrahem22@gmail.com>
2021-10-13 20:39:17 +0200

    adding isAdmin middleware

---
+++ middlewares/isAdmin.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/userModel");
+
+module.exports = async function (req, res, next) {
+  const token = req.header("authorization");
+  if (!token) return res.status(401).json("Access denied. No token provided");
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    let user = await User.findById(decode._id);
+    if (!user) return res.status(404).json("User Not found");
+
+    if (user.role !== "admin")
+      return res
+        .status(403)
+        .json("Forbidden, you don't have the access to these route");
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "invalid token", error: error.message });
+  }
+};
