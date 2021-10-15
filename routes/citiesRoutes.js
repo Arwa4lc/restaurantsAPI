1de9c867ed28f8f0213ed18e7791c7e766a6dbd5
Arwa Abdelrahem <arwaabdelrahem22@gmail.com>
2021-10-15 11:25:30 +0200

    cities routes swagger docs

--- routes/citiesRoutes.js
+++ routes/citiesRoutes.js
@@ -22,6 +22,28 @@ module.exports = router;
  *      200:
  *        description: success
  *
+ *  post:
+ *   summary: create new city account
+ *   description: create new city account
+ *   parameters:
+ *      - in: header
+ *        name: auth-token
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *   requestBody:
+ *      content:
+ *          application/json:
+ *              schema:
+ *                  $ref: "#definitions/City"
+ *   responses:
+ *      201:
+ *        description: new city created successfully
+ *      400:
+ *        description: failure in creating new city "invalid request body"
+ *
+ *
  * /cities/{id}:
  *  get:
  *   summary: get specific city by id
@@ -39,4 +61,54 @@ module.exports = router;
  *      404:
  *        description: city not found
  *
+ *  put:
+ *   summary: update specific city by id
+ *   description: update specific city by id
+ *   parameters:
+ *      - in: header
+ *        name: auth-token
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: User's access token.
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Numeric ID of the city to update
+ *   requestBody:
+ *      content:
+ *          application/json:
+ *              schema:
+ *                  $ref: "#definitions/City"
+ *   responses:
+ *      200:
+ *        description: success
+ *      404:
+ *        description: city not found
+ *
+ *  delete:
+ *   summary: delete specific city by id
+ *   description: delete specific city by id
+ *   parameters:
+ *      - in: header
+ *        name: auth-token
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: User's access token.
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Numeric ID of the city to delete
+ *   responses:
+ *      204:
+ *        description: no content
+ *      401:
+ *        description: Unauthorized "no token provided"
+ *      404:
+ *        description: city not found
  */
